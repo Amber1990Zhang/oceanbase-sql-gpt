72eaba81a53e21a4ceb4e5287f71f765445b531f
Amber1990Zhang <1345783682@qq.com>
2023-03-18 13:26:31 +0800

    update code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,27 +8,27 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your OceanBase SQL in seconds."
           />
-          <meta property="og:site_name" content="twitterbio.com" />
+          <meta property="og:site_name" content="sql.amber-moe.io" />
           <meta
             property="og:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your OceanBase SQL in seconds."
           />
-          <meta property="og:title" content="Twitter Bio Generator" />
+          <meta property="og:title" content="OceanBase SQL Generator" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
+          <meta name="twitter:title" content="OceanBase SQL Generator" />
           <meta
             name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
+            content="Generate your OceanBase SQL in seconds.
           />
           <meta
             property="og:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://sql.amber-moe.io/og-image.png"
           />
           <meta
             name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://sql.amber-moe.io/og-image.png"
           />
         </Head>
         <body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,33 +8,57 @@ import Footer from "../components/Footer";
 import Github from "../components/GitHub";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
+import { AnimatePresence, motion } from "framer-motion";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState("");
-  const [vibe, setVibe] = useState<VibeType>("Professional");
-  const [generatedBios, setGeneratedBios] = useState<String>("");
+  const [schema, setSchema] = useState("");
+  const [description, setDescription] = useState("");
+  const [sql, setSqls] = useState<String>("");
 
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  // console.log("Streamed response: ", sql);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+  const prompt =
+   `Generate OceanBase SQL with comments based on provided schema and description.
+   For OceanBase SQL Requirements:
 
-  const prompt = `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
-    vibe === "Funny"
-      ? "Make sure there is a joke in there and it's a little ridiculous."
-      : null
-  }
-      Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
-    bio.slice(-1) === "." ? "" : "."
-  }`;
+   1. If describing copywriting, you need to add '/*' in front and add '*/' in back to indicate annotation information.
+   2. DO NOT use SQL "SELECT ... FOR SHARE ..." syntax.
+   3. DO NOT use SQL "SHOW WARNINGS" syntax.
+   
 
-  const generateBio = async (e: any) => {
+   Input example:
+   ---
+   Schema:
+   table: Users (UserID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL).
+
+   Description:
+   Get 5 users whose first name contains 'Amber', case-insensitive.
+
+   SQL:
+
+   /* Get 5 users whose first name contains 'Amber', case-insensitive. */
+   SELECT * 
+   FROM Users 
+   WHERE LOWER(FirstName) LIKE '%amber%';
+   ---
+
+   Schema:
+   ${schema}
+   Description:
+   ${description}
+   Query:`;
+  
+  const composeQuery = async (e: any) => {
     e.preventDefault();
-    setGeneratedBios("");
+    setSqls("");
     setLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -45,6 +69,7 @@ const Home: NextPage = () => {
         prompt,
       }),
     });
+    console.log("Required SQL returned.");
 
     if (!response.ok) {
       throw new Error(response.statusText);
@@ -64,16 +89,15 @@ const Home: NextPage = () => {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
       const chunkValue = decoder.decode(value);
-      setGeneratedBios((prev) => prev + chunkValue);
+      setSqls((prev) => prev + chunkValue);
     }
-    scrollToBios();
     setLoading(false);
   };
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Bio Generator</title>
+        <title>OceanBase SQL generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -81,17 +105,17 @@ const Home: NextPage = () => {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
+          href="https://github.com/Amber1990Zhang/oceanbase-sql-gpt"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Github />
           <p>Star on GitHub</p>
         </a>
-        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using chatGPT
+        <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
+          Generate OceanBase SQL
         </h1>
-        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
+        {/* <p className="text-slate-500 mt-5">10,312 queries generated so far.</p> */}
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -102,36 +126,42 @@ const Home: NextPage = () => {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current bio{" "}
-              <span className="text-slate-500">
-                (or write a few sentences about yourself)
-              </span>
+              Write your schema
+              {/* <span className="text-slate-500">
+                (or drop your DDL queries)
+              </span> */}
               .
             </p>
           </div>
           <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
+            value={schema}
+            onChange={(e) => setSchema(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
+              "e.g. table: Users (UserID INT PRIMARY KEY AUTO_INCREMENT,FirstName VARCHAR(50) NOT NULL,LastName VARCHAR(50) NOT NULL,Email VARCHAR(100) NOT NULL UNIQUE,Password VARCHAR(255) NOT NULL)."
             }
           />
           <div className="flex mb-5 items-center space-x-3">
             <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
+            <p className="text-left font-medium">Describe what you would like to query.</p>
           </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              "e.g. Get 5 users whose first name contains 'Amber', case-insensitive."
+            }
+          />
 
           {!loading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
+              onClick={(e) => composeQuery(e)}
             >
-              Generate your bio &rarr;
+              Compose your Oceanbase SQL Query &rarr;
             </button>
           )}
           {loading && (
@@ -150,40 +180,46 @@ const Home: NextPage = () => {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <div className="space-y-10 my-10">
-          {generatedBios && (
-            <>
-              <div>
-                <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
-                >
-                  Your generated bios
-                </h2>
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
+          <AnimatePresence mode="wait">
+            <motion.div className="space-y-10 my-10">
+              {sql && (
+                <>
+                  <div>
+                    <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
+                      Your Queries
+                    </h2>
+                  </div>
+                  {/* text should be Left aligned */}
+                  <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto" style={{ textAlign: "left" }}>
+                  {sql
+                  .substring(sql.indexOf("1") + 3)
                   .split("2.")
-                  .map((generatedBio) => {
+                  .map((sql) => {
                     return (
                       <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
-                            icon: "✂️",
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
-                      </div>
+                      className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                      onClick={() => {
+                        navigator.clipboard.writeText(sql);
+                        toast("SQL copied to clipboard", {
+                          icon: "✂️",
+                        });
+                      }}
+                      key={sql}
+                    >
+                    <p>
+                    <SyntaxHighlighter language="sql" >
+                      {sql.toString()}
+                    </SyntaxHighlighter>
+                    </p>
+                    </div>
                     );
                   })}
-              </div>
-            </>
-          )}
-        </div>
+                  </div>
+                </>
+              )}
+            </motion.div>
+          </AnimatePresence>
+          </div>
       </main>
       <Footer />
     </div>
