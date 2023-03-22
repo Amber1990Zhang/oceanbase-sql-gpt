2637b070fbc2a013a82ded144d37b56795862d40
Amber Moe <42762957+Amber1990Zhang@users.noreply.github.com>
2023-03-22 16:56:02 +0800

    update ga code

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import ReactGA from 'react-ga';
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
 
+function Header() {
+  const router = useRouter();
 
-export default function Header() {
   useEffect(() => {
-    // GA跟踪代码
-    window.gtag("config", "G-DEWJBLD2BG");
-  }, []);
+    // Initialize Google Analytics with your tracking ID
+    ReactGA.initialize('G-DEWJBLD2BG');
+
+    // Record a pageview for the current page
+    ReactGA.pageview(router.pathname);
+  }, [router.pathname]);
+
   return (
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <Link href="/" className="flex space-x-3">
